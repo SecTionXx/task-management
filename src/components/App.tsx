313a8d8fe455e44a192a24b54e7bde4bc78b0ab9
Punt Boworn.T <boworn.tr@gmail.com>
2023-10-09 00:02:26 +0700

    Add Daisy UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div className="flex justify-center bg-gray-300">Hello, world</div>
+  return (
+    <div className=" flex justify-center bg-gray-300 text-red-600">
+      Hello, world
+      <button className="btn-primary btn">Test</button>
+    </div>
+  )
 }
 
 export default App
